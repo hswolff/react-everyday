e1517c27c6d78ac1a3941958cdf4197051cabbd5
Harry Wolff <hello@hswolff.com>
2018-09-26 10:13:30 -0400

    fix: Don't throw an error when deleting a project

--- components/ProjectScreen.tsx
+++ components/ProjectScreen.tsx
@@ -39,7 +39,8 @@ export default class ProjectScreen extends React.Component<Props> {
     );
     return (
       <Consumer select={[selectors.getProject(projectName)]}>
-        {(project: Project) => (
+        {(project: Project) =>
+          project ? (
           <View>
             <CalendarList
               // Max amount of months allowed to scroll to the past. Default = 50
@@ -87,7 +88,8 @@ export default class ProjectScreen extends React.Component<Props> {
               }}
             />
           </View>
-        )}
+          ) : null
+        }
       </Consumer>
     );
   }
